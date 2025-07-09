9e77e943d08a30f71ce30d79a280e196e7ac6526
MilanLR <milan@lariviereweb.com>
2025-07-09 10:10:23 +0200

    marktplaats scrapeSearchPage

---
+++ platforms.ts/marktplaats.ts
@@ -0,0 +1,32 @@
+import { Page } from "puppeteer";
+import { Listing, Platform } from "./base";
+
+export class Marktplaats implements Platform {
+  name = "marktplaats";
+
+  async scrapeSearchPage(
+    page: Page,
+    keyword: string,
+    limit: number
+  ): Promise<string[]> {
+    let pageNumber = 1;
+    const urls: string[] = [];
+
+    while (urls.length < limit) {
+      const url = `https://www.marktplaats.nl/q/${keyword}/p/${pageNumber}/`;
+      await page.goto(url);
+      const items = await page.$$eval("a.hz-Listing-coverLink", (items) => {
+        return items.map((item) => {
+          return "https://www.marktplaats.nl" + item.href;
+        });
+      });
+      urls.push(...items);
+      pageNumber++;
+    }
+    return urls;
+  }
+
+  async scrapeItemPage(): Promise<Listing> {
+    throw new Error("Not implemented");
+  }
+}
